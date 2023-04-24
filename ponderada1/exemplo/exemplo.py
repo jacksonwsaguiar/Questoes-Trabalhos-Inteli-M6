2a1fb2534d5108b7a784051b9766e4f65f8693a4
jackson aguiar <jacksonwsaguiar>
2023-04-24 20:52:40 -0300

    first commit: weighted question 1

--- ponderada1/exemplo/exemplo.py
+++ ponderada1/exemplo/exemplo.py
@@ -2,19 +2,42 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import time
 
 
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
-        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 20)
         self.timer_ = self.create_timer(0.1, self.move_turtle)
         self.twist_msg_ = Twist()
 
     def move_turtle(self):
-        self.twist_msg_.linear.x = 1.0
-        self.twist_msg_.angular.z = 0.5
+
+        for i in range(2):
+            self.twist_msg_.angular.z = 1.56
+            self.twist_msg_.linear.x = 1.0
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(1)
+
+            self.twist_msg_.linear.x = 1.0
+            self.twist_msg_.angular.z = 0.0
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(1)
+            self.twist_msg_.linear.x = 1.0
+            self.twist_msg_.angular.z = 0.0
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(1)
+            self.twist_msg_.angular.z = 1.56
+            self.twist_msg_.linear.x = 1.0
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(1)
+            
+
+        self.twist_msg_.angular.z = 1.0
+        self.twist_msg_.linear.x = -0.5
         self.publisher_.publish(self.twist_msg_)
+        time.sleep(1)
 
 
 def main(args=None):
